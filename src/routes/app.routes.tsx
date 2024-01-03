ca88dff8ea47177cbdeabbd3c0659fc1a0b756d5
Welison Wessling <wwelisonw@gmail.com>
2024-01-03 14:35:21 -0300

    feat: stylized Tab Bar

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,28 +1,83 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  createBottomTabNavigator,
+  BottomTabNavigationProp,
+} from "@react-navigation/bottom-tabs";
+import { useTheme } from "native-base";
+import { Platform } from "react-native";
 import { Home } from "@screens/Home/Home";
 import { History } from "@screens/History/History";
 import { Exercise } from "@screens/Exercise/Exercise";
 import { Profile } from "@screens/Profile/Profile";
+import HomeSVG from "@assets/home.svg";
+import HistorySVG from "@assets/history.svg";
+import ProfileSVG from "@assets/profile.svg";
 
-const { Navigator, Screen } = createBottomTabNavigator();
+type AppRoutes = {
+  home: undefined;
+  history: undefined;
+  exercise: undefined;
+  profile: undefined;
+};
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export const AppRoutes = () => {
-  <Navigator>
-    <Screen 
-      name="home" 
-      component={Home} 
-    />
-    <Screen 
-      name="history" 
-      component={History} 
-    />
-    <Screen 
-      name="exercise" 
-      component={Exercise} 
-    />
-    <Screen 
-      name="profile" 
-      component={Profile} 
-    />
-  </Navigator>;
+  const { sizes, colors } = useTheme();
+
+  const iconSize = sizes[6];
+
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.green[500],
+        tabBarInactiveTintColor: colors.gray[300],
+        tabBarStyle: {
+          backgroundColor: colors.gray[600],
+          borderTopWidth: 0,
+          height: Platform.OS === "ios" ? 96 : "auto",
+          paddingBottom: sizes[10],
+          paddingTop: sizes[6],
+        },
+      }}
+    >
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSVG fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen
+        name="history"
+        component={History}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HistorySVG fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen
+        name="exercise"
+        component={Exercise}
+        options={{
+          tabBarButton: () => null,
+        }}
+      />
+      <Screen
+        name="profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ProfileSVG fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+    </Navigator>
+  );
 };
