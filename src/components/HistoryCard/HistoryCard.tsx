3d8423d103ba20b6f28b441da8425a45343c55db
Welison Wessling <wwelisonw@gmail.com>
2024-01-04 14:38:48 -0300

    feat: created HistoryCard component

---
+++ src/components/HistoryCard/HistoryCard.tsx
@@ -0,0 +1,40 @@
+import { HStack, Heading, Text, VStack } from "native-base";
+
+export const HistoryCard = () => {
+  return (
+    <HStack
+      alignItems="center"
+      justifyContent="space-between"
+      w="full"
+      bgColor="gray.600"
+      px={5}
+      py={4}
+      mb={3}
+      rounded="md"
+    >
+      <VStack>
+        <Heading
+          color="white"
+          fontFamily="heading"
+          fontSize="md"
+          textTransform="capitalize"
+        >
+          Costas
+        </Heading>
+        <Text
+          color="gray.100"
+          fontFamily="body"
+          fontSize="lg"
+          mt={1}
+          numberOfLines={1}
+        >
+          Puxada frontal
+        </Text>
+      </VStack>
+
+      <Text color="gray.300" fontFamily="body" fontSize="md">
+        08:56
+      </Text>
+    </HStack>
+  );
+};
--- src/screens/History/History.tsx
+++ src/screens/History/History.tsx
@@ -1,9 +1,57 @@
-import { Center, Text } from "native-base";
+import { HistoryCard } from "@components/HistoryCard/HistoryCard";
+import { ScreenHeader } from "@components/ScreenHeader/ScreenHeader";
+import { Heading, SectionList, Text, VStack } from "native-base";
+import { useState } from "react";
 
 export const History = () => {
+  const [exercises, setExercises] = useState([
+    {
+      title: "28.08.22",
+      data: ["Puxada Frontal", "Remada unilateral"],
+    },
+    {
+      title: "26.08.22",
+      data: ["Puxada Frontal"],
+    },
+  ]);
+
   return (
-    <Center flex={1}>
-      <Text>History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de Exercícios" />
+
+      {/* Histórico dos exercícios feitos */}
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderSectionHeader={({ section }) => (
+          <Heading
+            color="gray.200"
+            fontFamily="heading"
+            fontSize="md"
+            mt={10}
+            mb={3}
+          >
+            {section.title}
+          </Heading>
+        )}
+        renderItem={({ item }) => <HistoryCard />}
+        contentContainerStyle={
+          !exercises.length && { flex: 1, justifyContent: "center" }
+        }
+        ListEmptyComponent={() => (
+          <Text
+            color="gray.100"
+            fontFamily="body"
+            fontSize="md"
+            textAlign="center"
+          >
+            Não há exercícios registrados ainda.{"\n"}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+        px={6}
+      />
+    </VStack>
   );
 };
