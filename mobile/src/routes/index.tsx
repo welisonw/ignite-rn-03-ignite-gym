4cfb6784acc75960eca6780c4a6e5fde07140149
Welison Wessling <wwelisonw@gmail.com>
2024-01-10 14:25:44 -0300

    feat: redirected logged user

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,9 +1,14 @@
-import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
-import { AuthRoutes } from "./auth.routes";
-import { Box, useTheme } from "native-base";
+import {DefaultTheme, NavigationContainer} from "@react-navigation/native";
+import {AuthRoutes} from "./auth.routes";
+import {AppRoutes} from "./app.routes";
+import {Box, useTheme} from "native-base";
+import {useAuthContext} from "@contexts/AuthContext";
 
 export const Routes = () => {
-  const { colors } = useTheme();
+  const {colors} = useTheme();
+  const {user} = useAuthContext();
+
+  console.log(user);
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
@@ -11,7 +16,7 @@ export const Routes = () => {
   return (
     <Box flex={1} bgColor="gray.700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
