bc1938a9e6c21d5257f46e0af72175dea797593f
Welison Wessling <wwelisonw@gmail.com>
2024-01-10 16:04:38 -0300

    feat: created storageUser.ts

---
+++ mobile/src/storage/storageUser.ts
@@ -0,0 +1,15 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { USER_STORAGE } from "./storageConfig";
+import { UserDTO } from "@dtos/UserDTO";
+
+export const storageUserSave = async (user: UserDTO) => {
+  await AsyncStorage.setItem(USER_STORAGE, JSON.stringify(user));
+};
+
+export const storageUserGet = async () => {
+  const storaged = await AsyncStorage.getItem(USER_STORAGE);
+
+  const user: UserDTO = storaged ? JSON.parse(storaged) : {};
+
+  return user;
+};
