e29ff37a905a4a98ce450f2acbd15d20853a9ad9
Welison Wessling <wwelisonw@gmail.com>
2024-01-11 17:37:30 -0300

    feat: removing the token from storage

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -14,6 +14,7 @@ import {
 } from "@storage/storageUser";
 import {
   storageAuthTokenGet,
+  storageAuthTokenRemove,
   storageAuthTokenSave,
 } from "@storage/storageAuthToken";
 
@@ -65,6 +66,7 @@ export const AuthContextProvider = ({children}: PropsWithChildren) => {
       setUser({} as UserDTO);
 
       await storageUserRemove();
+      await storageAuthTokenRemove();
     } catch (error) {
       throw error;
     } finally {
