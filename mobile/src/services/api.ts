6d648a281b0889df0758ed15e4632e83536da759
Welison Wessling <wwelisonw@gmail.com>
2024-01-16 15:28:51 -0300

    feat: implemented the resending of failed requests (failedQueue) using the new token obtained from the API.

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -57,9 +57,21 @@ api.registerInterceptTokenManager = signOut => {
           try {
             const { data } = await api.post("/sessions/refresh-token", { refresh_token });
 
-            console.log(data)
-
             await storageAuthTokenSave(data.token, data.refresh_token);
+
+            if (originalRequestConfig.data) {
+              originalRequestConfig.data = JSON.parse(originalRequestConfig.data);
+            };
+
+            originalRequestConfig.headers = { "Authorization": `Bearer ${data.token}` };
+
+            api.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
+
+            failedQueue.forEach(request => {
+              request.onSucess(data.token);
+            });
+
+            resolve(api(originalRequestConfig));
           } catch (error: any) {
             failedQueue.forEach(request => request.onFailure(error))
 
